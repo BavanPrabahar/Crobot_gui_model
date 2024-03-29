9072f280c83675e3b155ac09664bac2bbef8a444
Your Name <you@example.com>
2024-03-29 16:51:30 +0530

    changes made

--- gui.py
+++ gui.py
@@ -7,7 +7,6 @@ import time
 from kivy.clock import Clock
 from datetime import datetime
 from kivy.uix.popup import Popup
-from pulsectl import Pulse, PulseVolumeInfo
 import subprocess
 import alsaaudio
 
@@ -31,7 +30,7 @@ Colors = {
     },
     "Light": {
         "StatusBar": "E0E0E0",
-        "AppBar": "#202020",
+        "AppBar": "#EEEEEE",
         "Background": "#2E3032",
         "CardsDialogs": "#FFFFFF",
         "FlatButtonDown": "#CCCCCC",
@@ -43,6 +42,8 @@ Colors = {
 
 
 class FirstKVfileApp(MDApp): 
+    x=0
+     
 
     def build(self):
 
@@ -70,8 +71,8 @@ class FirstKVfileApp(MDApp):
         current_date = now.strftime("%d-%m-%Y")
 
         # Update labels
-        self.root.ids.time_label.text = f"Time: {current_time}"
-        self.root.ids.date_label.text = f"Date: {current_date}"
+        #elf.root.ids.time_label.text = f"Time: {current_time}"
+        #self.root.ids.date_label.text = f"Date: {current_date}"
 
     def show_volume_slider_1(self):
         volume_slider_1 = self.root.ids.volume_slider_1
@@ -81,27 +82,77 @@ class FirstKVfileApp(MDApp):
     def show_volume_slider_2(self):
         volume_slider_2=self.root.ids.volume_slider_2
         volume_slider_2.opacity = 1
-        
-    def btn1(self):
-    	catkin_workspace_path = "/home/crobot/crobot_ws/devel/setup.bash"
-    	command1 = f"source {catkin_workspace_path} && roslaunch hello12 firsta.launch"
-    	full_command = f"gnome-terminal --tab --title='Tab 1' --command='bash -c \"{command1}; exec bash\"'"
-    	subprocess.Popen(full_command, shell=True)
+
+    def show_volume_slider_3(self):
+        volume_slider_3=self.root.ids.volume_slider_3
+        volume_slider_3.opacity = 1
+
+    def show_volume_slider_4(self):
+        volume_slider_4=self.root.ids.volume_slider_4
+        volume_slider_4.opacity = 1
+
+    def show_volume_slider_5(self):
+        volume_slider_5=self.root.ids.volume_slider_5
+        volume_slider_5.opacity = 1
+
+   
     def on_slider_value_change(self, value):
     	slider_value_1=int(value)
-    	print(slider_value_1)
+       
     	m = alsaaudio.Mixer()
-    	m.setvolume(slider_value_1)
-    def on_slide_1(self,value):
-    	abc=int(value)
-    	self.ids["volume_slider_1"].value2=abc
+    	m.setvolume(slider_value_1)  
+    	x=m.getvolume()
     	
- 
-		
-
+    def on_slider2(self,value):
+    	slider_value_2=int(value)
+       
+    	m = alsaaudio.Mixer()
+    	m.setvolume(slider_value_2)  
+    	x=m.getvolume()
+    	self.root.ids["volume_slider_1"].value=x[0]
+    	
+    	
+    	
+    	
+    def btn1(self):
+    
+    	catkin_workspace_path = "/home/racer/catkin_ws/devel/setup.bash"
+    	command1 = f"source {catkin_workspace_path} && roslaunch hello a.launch"
+    	
+    	full_command = f"gnome-terminal --tab --title='Tab 1' --command='bash -c \"{command1}; exec bash\"'"
+    	subprocess.Popen(full_command, shell=True)  
+    def btn5(self):
+    
+    	catkin_workspace_path = "/home/racer/catkin_ws/devel/setup.bash"
+    	command1 = f"source {catkin_workspace_path} && roslaunch hello b.launch"
+    	
+    	full_command = f"gnome-terminal --tab --title='Tab 1' --command='bash -c \"{command1}; exec bash\"'"
+    	subprocess.Popen(full_command, shell=True)  
+    def btn3(self):
+    
+    	catkin_workspace_path = "/home/racer/catkin_ws/devel/setup.bash"
+    	command1 = f"source {catkin_workspace_path} && sudo shutdown now"
+    	
+    	full_command = f"gnome-terminal --tab --title='Tab 1' --command='bash -c \"{command1}; exec bash\"'"
+    	subprocess.Popen(full_command, shell=True)  
+    	
+    	
+    def btn2(self):
+    
+    	catkin_workspace_path = "/home/racer/catkin_ws/devel/setup.bash"
+    	command2 = f"source {catkin_workspace_path} && rosnode kill webcam_pub webcam_sub"
+    	full_command = f"gnome-terminal --tab --title='Tab 1' --command='bash -c \"{command2}; exec bash\"'"
+    	subprocess.Popen(full_command, shell=True)  
+    
+    def btn4(self):
+    	catkin_workspace_path = "/home/racer/catkin_ws/devel/setup.bash"
+    	command2 = f"source {catkin_workspace_path} && rosnode kill -a"
+    	full_command = f"gnome-terminal --tab --title='Tab 1' --command='bash -c \"{command2}; exec bash\"'"
+    	subprocess.Popen(full_command, shell=True)  
+    	
 
 
 if __name__=="__main__":
-    Window.borderless=True
-    Config.set('kivy','exit_on_escape','0')
+    # Window.borderless=True
+
     FirstKVfileApp().run()
